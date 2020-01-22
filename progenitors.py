7f4bdf625f94be995dde5c60ad3dd63b0c887a35
Zac Johnston <zac.johnston@monash.edu>
2020-01-22 16:02:06 -0500

    [progenitors.py] added some quick plotting functions

--- progenitors.py
+++ progenitors.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 prog_path = '/Users/zac/projects/data/progenitors/sukhbold_2018/mdotone'
 
+
 def load_prog(mass, path=None, skiprows=3):
     """Load progenitor model
 
@@ -38,3 +39,71 @@ def prog_filepath(mass, path=None):
     filepath = os.path.join(path, filename)
 
     return filepath
+
+
+# ===================================================
+#           Plotting
+# ===================================================
+def plot_ye(prog, ye, vline=None):
+    fig, ax = plt.subplots(figsize=[8, 6])
+
+    ax.plot(prog['radius'], prog['Ye'], label='Provided')
+    ax.plot(prog['radius'], ye, label='Calculated')
+    add_vline(ax, vline=vline, plot_type='ye')
+
+    ax.set_xscale('log')
+    ax.set_ylabel('Ye')
+    ax.set_xlabel('radius (cm)')
+    ax.legend()
+
+    return fig, ax
+
+
+def plot_abar(prog, abar, vline=None):
+    fig, ax = plt.subplots(figsize=[8, 6])
+
+    ax.plot(prog['radius'], prog['Abar'], label='Provided')
+    ax.plot(prog['radius'], abar, label='Calculated')
+    add_vline(ax, vline=vline, plot_type='abar')
+
+    ax.set_xscale('log')
+    ax.set_ylabel('Abar')
+    ax.set_xlabel('radius (cm)')
+    ax.legend()
+
+    return fig, ax
+
+
+def plot_x(prog, net, cr56=None, vline=None):
+    fig, ax = plt.subplots(figsize=[8, 6])
+
+    for row in net.itertuples():
+        iso = row.isotope.capitalize()
+        if iso == 'N':
+            iso = 'neutrons'
+        ax.plot(prog['radius']/1e5, prog[iso], label=iso)
+
+    if cr56 is not None:
+        ax.plot(prog['radius']/1e5, cr56, label='cr56')
+
+    add_vline(ax, vline=vline, plot_type='x')
+
+    ax.set_xscale('log')
+    ax.set_ylabel('X')
+    ax.set_xlabel('radius (km)')
+    ax.legend()
+
+    return fig, ax
+
+
+def add_vline(ax, vline, plot_type):
+    if vline is not None:
+        ylims = None
+
+        if plot_type in ('x', 'ye'):
+            ylims = [0, 1]
+        elif plot_type in ('abar',):
+            ylims = [0, 60]
+
+        ax.vlines(vline, ylims[0], ylims[1], ls='--', color='k')
+
